9899aec9b3932a753737fa5695b0cbaf2690d164
notamoniac <notamoniac@gmail.com>
2026-08-10 00:11:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,20 +11,21 @@ var count = function(){
 
 function show(){
     let show = document.getElementById("show-here");
+    let show1 = document.getElementById("show-here1");
     show.innerHTML = "This button is working in progress so it's useless to click it." ;
     count()
-    show.innerHTML += "<br> You have clicked the button " + counter + " times.";
+    //show.innerHTML += "<br> You have clicked the button " + counter + " times.";
     if(counter > 5){
-        show.innerHTML += "<br> You have clicked the button more than 5 times!";
+        show1.innerHTML = "<br> You have clicked the button more than 5 times!";
     }
     if(counter > 20){
-        show.innerHTML += "<br> You have clicked the button more than 20 times!";
+        show1.innerHTML = "<br> You have clicked the button more than 20 times!";
     }
     if(counter > 60){
-        show.innerHTML += "<br> If you have nothing better to do, you can keep clicking the button.";
+        show1.innerHTML = "<br> If you have nothing better to do, you can keep clicking the button.";
     }
     if(counter > 100){
-        show.innerHTML += "<br> Ok";
+        show1.innerHTML = "<br> Ok";
     }
     if(counter > 109){ 
         window.open("https://notamoniac.github.io/about-me/videos/nggyu.mp4", "_blank");
